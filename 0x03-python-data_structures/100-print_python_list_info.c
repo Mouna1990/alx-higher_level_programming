6d960ca54d7720d9bf6b076c37e492f7c4dc9aa5
mouna <mmenouer11@gmail.com>
2023-07-15 23:54:38 -0700

    tasks 7 to 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,49 +1,25 @@
-#include <stdio.h>
-#include "list.h"
+#include <Python.h>
 
-void print_python_list_info(List* p)
+/**
+* print_python_list_info - Prints basic info about Python lists.
+* @p: A PyObject list.
+*/
+void print_python_list_info(PyObject *p)
 {
-size_t size = list_size(p);
+int size, alloc, i;
+PyObject *obj;
 
-printf("[*] Size of the Python List = %lu\n", size);
-printf("[*] Allocated = %lu\n", p->allocated);
+size = Py_SIZE(p);
+alloc = ((PyListObject *)p)->allocated;
 
-for (size_t i = 0; i < size; i++)
+printf("[*] Size of the Python List = %d\n", size);
+printf("[*] Allocated = %d\n", alloc);
+
+for (i = 0; i < size; i++)
 {
-void* item = list_get(p, i);
-const char* item_type = item ? "unknown" : "None";
+printf("Element %d: ", i);
 
-if (item)
-{
-item_type = "str";
-if (sizeof(int) == sizeof(item))
-item_type = "int";
-else if (sizeof(float) == sizeof(item))
-item_type = "float";
-else if (sizeof(double) == sizeof(item))
-item_type = "double";
+obj = PyList_GetItem(p, i);
+printf("%s\n", Py_TYPE(obj)->tp_name);
 }
-
-printf("Element %lu: %s\n", i, item_type);
-}
-}
-int main()
-{
-List* l = list_create();
-
-list_add(l, "hello");
-list_add(l, "World");
-print_python_list_info(l);
-
-list_remove(l, 1);
-print_python_list_info(l);
-
-list_add(l, "My string");
-list_add(l, NULL);
-list_add(l, "Another string");
-print_python_list_info(l);
-
-list_destroy(l);
-
-return (0);
 }
