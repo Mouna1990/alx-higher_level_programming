7dac7dee5b9a439bf7ab39048bc192b58ebbec79
mouna <mmenouer11@gmail.com>
2023-07-15 23:48:10 -0700

    tasks 7 to 14

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,6 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
- 
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
@@ -13,5 +12,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
